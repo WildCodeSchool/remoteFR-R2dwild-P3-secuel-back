d4691dbb5ca95ca6983c1d41cc2a0ec33de1d0f7
Christellefz <christellefz@yahoo.fr>
2021-03-17 16:19:54 +0100

    implementation des routes notifs

--- routes/notif_insured.js
+++ routes/notif_insured.js
@@ -5,8 +5,8 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   connection.query(
-    'SELECT * FROM Specialities',
-    [req.params.id],
+    'SELECT * FROM notif_insured JOIN Insured ON Insured_id_Assure= id_insured JOIN Notifications ON Notifications_id_Notification = id_Notification ',
+    [req.para],
     (err, results) => {
       if (err) {
         console.log(err)
@@ -18,9 +18,10 @@ router.get('/', (req, res) => {
   )
 })
 
-router.get('/:id', (req, res) => {
+
+router.get('/:id/:id', (req, res) => {
   connection.query(
-    'SELECT * FROM Specialities WHERE id_speciality = ?',
+    'SELECT * FROM notif_insured WHERE Notifications_id_Notification = ? AND Insured_id_Assure= ?',
     [req.params.id],
     (err, results) => {
       if (err) {
@@ -37,28 +38,28 @@ router.post(
   '/',
 
   (req, res) => {
-    const { speciality_name } = req.body
+    const { Notifications_id_Notification, Insured_id_Assure } = req.body
     return connection.query(
-      'INSERT INTO specialities(speciality_name) VALUES(?)',
-      [speciality_name],
+      'INSERT INTO notif_insured(Notifications_id_Notification, Insured_id_Assure) VALUES(?,?)',
+      [Notifications_id_Notification, Insured_id_Assure],
       err => {
         if (err) {
           console.log(err)
-          return res.status(500).send('Error saving speciality')
+          return res.status(500).send('Error saving notif insured')
         }
-        return res.status(200).send('Successfully saved speciality')
+        return res.status(200).send('Successfully saved notif insured')
       }
     )
   }
 )
 
-router.put('/:id', (req, res) => {
-  const idSpe = req.params.id
-  const newSpe = req.body
+router.put('/:id/:id', (req, res) => {
+  const idNotifIns = req.params.id
+  const newNotifIns = req.body
 
   return connection.query(
-    'UPDATE specialities SET ? WHERE id_speciality = ?',
-    [newSpe, idSpe],
+    'UPDATE notif_insured SET ? WHERE Notifications_id_Notification = ? AND Insured_id_Assure= ?',
+    [newNotifIns, idNotifIns],
     err2 => {
       if (err2) {
         return res.status(500).json({
@@ -68,8 +69,8 @@ router.put('/:id', (req, res) => {
       }
 
       connection.query(
-        'SELECT * FROM specialities WHERE id_speciality = ?',
-        idSpe,
+        'SELECT * FROM notif_insured WHERE Notifications_id_Notification = ? AND Insured_id_Assure= ?',
+        idNotifIns,
         (err3, records) => {
           if (err3) {
             res.status(500).json({
@@ -78,30 +79,30 @@ router.put('/:id', (req, res) => {
             })
           }
 
-          const updatedSpe = records[0]
-          const { ...specialities } = updatedSpe
+          const updatedNotifIns = records[0]
+          const { ...notif_insured } = updatedNotifIns
           // Get the host + port (localhost:3000) from the request headers
           const host = req.get('host')
           // Compute the full location, e.g. http://localhost:3000/api/users/132
           // This will help the client know where the new resource can be found!
-          const location = `http://${host}${req.url}/${specialities.id}`
-          return res.status(201).set('Location', location).json(specialities)
+          const location = `http://${host}${req.url}/${notif_insured.id}`
+          return res.status(201).set('Location', location).json(notif_insured)
         }
       )
     }
   )
 })
-//delete 
-router.delete('/:id', (req, res) => {
+//delete
+router.delete('/:id/:id', (req, res) => {
   connection.query(
-    'DELETE FROM specialities WHERE id_speciality = ?',
-    [req.params.id],
-    (err) => {
+    'DELETE FROM notif_insured WHERE Notifications_id_Notification = ? AND Insured_id_Assure= ?',
+    [req.params.id, req.params.id],
+    err => {
       if (err) {
         console.log(err)
         res.status(500).send('Error deleting data')
       } else {
-        res.status(200).send("Speciality sucessfuly deleted !")
+        res.status(200).send('Speciality sucessfuly deleted !')
       }
     }
   )
