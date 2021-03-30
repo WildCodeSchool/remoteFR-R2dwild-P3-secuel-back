ab35b816ee83a231e0d0cf9cbeeaeb5ff83e2a42
Jeromerobichez <jerome.robichez@gmail.com>
2021-03-30 15:51:29 +0200

    correction index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const app = express()
+
 const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 const routes = require('./routes/index')
+
+const app = express()
 
 app.use(cors('*'))
 app.use(express.json())
