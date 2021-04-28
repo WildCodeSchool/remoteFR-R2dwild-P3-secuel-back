0e6db5b4d74a6dd311a9ec241039f5856987d3e2
xenogearsX <xenogears@hotmail.fr>
2021-04-28 10:37:21 +0200

    eslint + database correction

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST, // address of the server
   user: process.env.DB_USER, // username
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME
 })
 
-module.exports = connection;
+module.exports = connection
